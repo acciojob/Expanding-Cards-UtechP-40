95714f029b21300702d60bb1b973030c9d9976a1
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-10-04 02:39:15 +0530

    Attempt:1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let cards = document.querySelectorAll(".panel")
+let previous = document.querySelector(".active")
+let first = previous;
+for(let card of cards){
+    card.addEventListener("mouseover",(e)=>{
+        previous.className = "panel"
+        console.log("hello")
+        card.className = "panel active"
+        previous=card;
+    })
+}
+
